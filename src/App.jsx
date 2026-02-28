aaf5e8003d18e258eac1bbbfa53b1573b322c535
Prashant Bhadane <pkbhadane545@gmail.com>
2026-02-28 21:54:57 +0530

    Initial setup of VidTube frontend

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button, buttonVariants } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' text-3xl bg-sky-300'>VidTube</h1>
+      <Button variant='destructive'>Click Me!</Button>
+      
+    </>
+  )
+}
+
+export default App
