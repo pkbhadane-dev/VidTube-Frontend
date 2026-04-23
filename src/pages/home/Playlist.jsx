37cac5c7a625b0b4960d4f44627d71090569fc55
Prashant Bhadane <pkbhadane545@gmail.com>
2026-04-23 11:48:34 +0530

    fix playlist page logic empty playlist

--- src/pages/home/Playlist.jsx
+++ src/pages/home/Playlist.jsx
@@ -1,23 +1,20 @@
-
 import { PlaylistCard } from "@/components/playlist-card";
 import { useFetchPlaylist } from "@/hooks/usePlaylist";
 import { useAuthStore } from "@/store/useAuthStore";
 
 export const Playlist = () => {
+  const { user } = useAuthStore();
 
-  const {user} = useAuthStore()
-
-  const {data:playlists, isPending} = useFetchPlaylist(user?._id)
-  console.log(playlists);
-  
+  const { data: playlists } = useFetchPlaylist(user?._id);
 
   return (
     <>
-       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
-            {playlists?.map((playlist) => (
-              <PlaylistCard key={playlist._id} playlist={playlist} />
-            ))}
-          </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
+        {playlists?.map((playlist) => (
+          <PlaylistCard key={playlist._id} playlist={playlist} />
+        ))}
+      </div>
+         {playlists?.length > 0 || <div className="text-xl text-center font-semibold text-purple-400">You don't have any playlist</div>}
     </>
   );
 };
