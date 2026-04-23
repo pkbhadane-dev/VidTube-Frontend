e7c7f4048026d564f23fa9692372f84e08dd0b61
Prashant Bhadane <pkbhadane545@gmail.com>
2026-04-23 11:55:24 +0530

    fix some styling

--- src/components/playlist-card.jsx
+++ src/components/playlist-card.jsx
@@ -9,7 +9,7 @@ export const PlaylistCard = ({ playlist }) => {
         <div className="relative aspect-video rounded-xl overflow-hidden bg-zinc-800">
           <img
             src={playlist?.videoDetails?.thumbnail || "/default-playlist.jpg"}
-            className="w-full h-full object-cover group-hover:scale-105 transition-transform"
+            className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
           />
 
           <div className="absolute inset-y-0 right-0 w-1/3 bg-black/60 backdrop-blur-[2px] flex flex-col items-center justify-center text-white">
